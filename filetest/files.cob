9a87ebcde57fa8c00929a18858f3a8587bc74ed7
legop3 <minecratfd8@gmail.com>
2023-01-16 01:59:39 -0500

    stuff

--- filetest/files.cob
+++ filetest/files.cob
@@ -13,6 +13,11 @@ environment division.
        01 student-file.
            05 student-id pic 9(5).
            05 name pic A(25).
+           05 flags.    
+              10 Flag1 pic a.
+              10 Flag2 pic a.
+              10 Flag3 pic a.
+
 
        working-storage section.
        01 ws-student.
@@ -27,7 +32,11 @@ procedure division.
                read student into ws-student
                    at end move 'Y' to ws-eof
                    not at end display ws-student
+                   display ws-name
+                   display flags
+                
                end-read
            end-perform.
        close student.
+       bell.
 stop run.
