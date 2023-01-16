a05f9d7db3ec241deceff3335cd58fdee2f36116
legop3 <minecratfd8@gmail.com>
2023-01-16 00:31:32 -0500

    we

--- test.cob
+++ test.cob
@@ -3,4 +3,8 @@ PROGRAM-ID. HELLO-WORLD.
 *> simple hello world program
 PROCEDURE DIVISION.
        DISPLAY 'Hello world!'.
+       
+       PERFORM 1000-SHOWIT 5 TIMES.
        STOP RUN.
+1000-SHOWIT.
+       DISPLAY 'Maybe'.
